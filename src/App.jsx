c5cf64ae62152b9b300706ad4a9011f4d9c6dbd5
manojsravan <manojsravan2002@gmail.com>
2024-05-17 15:14:40 +0530

    added import React, { createContext, useState, useContext } from 'react';

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login'
 import Dashboardmain from './Components/Dashboard Components/Dashboardmain'
 import Nopage from './Components/Error Page/Nopage';
+import ProtectedRoute from './Components/ProtectedRoute';
+import Dashboard from './Components/Homepage Components/Dashboard';
 
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboardmain />} />
         <Route path="*" element={<Nopage />} />
-
+        <ProtectedRoute component={Dashboard} />
         
        
       </Routes>
