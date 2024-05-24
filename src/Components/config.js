1f6c7976396dfac68c4c309752e18c269a2f96eb
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-24 11:42:33 +0530

    dsadad

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: 'https://qubinest-backend-five.vercel.app', // Production URL
-    // apiUrl: 'http://localhost:9988', // Localhost URL
+    // apiUrl: 'https://qubinest-backend-five.vercel.app', // Production URL
+    apiUrl: 'http://localhost:9988', // Localhost URL
   };
   
   export default config;
