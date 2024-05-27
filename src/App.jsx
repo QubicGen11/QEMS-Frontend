b321ae2981b86c68372fc3a353dd8945adc83dcb
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-27 12:56:07 +0530

    Added route

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <>
         <BrowserRouter>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboardmain />} />
             <Route path="/viewtimesheets" element={<ViewTimesheets />} />
