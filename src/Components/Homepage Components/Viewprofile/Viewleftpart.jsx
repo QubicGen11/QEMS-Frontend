cdcb377a98449284885e56b0247d73709d978330
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-28 00:40:13 +0530

    Updated profile picture for view profile

--- src/Components/Homepage Components/Viewprofile/Viewleftpart.jsx
+++ src/Components/Homepage Components/Viewprofile/Viewleftpart.jsx
@@ -9,7 +9,7 @@ const Viewleftpart = () => {
                                         <div className="text-center" bis_skin_checked={1}>
                                             <img
                                                 className="profile-user-img img-fluid img-circle"
-                                                src="../../distingg/img/user4-128x128.jpg"
+                                                src="https://res.cloudinary.com/defsu5bfc/image/upload/v1716836865/IMG_20231030_105454_660_x6loyi.jpg"
                                                 alt="User profile picture"
                                             />
                                         </div>
@@ -44,24 +44,23 @@ const Viewleftpart = () => {
                                             <i className="fas fa-book mr-1" /> Education
                                         </strong>
                                         <p className="text-muted">
-                                            B.S. in Computer Science from the University of Tennessee at
-                                            Knoxville
+                                            B.Tech in Electrical and Electronics from the  Srinivasa Ramaujan Institue of Technology
                                         </p>
                                         <hr />
                                         <strong>
                                             <i className="fas fa-map-marker-alt mr-1" /> Location
                                         </strong>
-                                        <p className="text-muted">Malibu, California</p>
+                                        <p className="text-muted p-2">Anantapur, AP</p>
                                         <hr />
                                         <strong>
                                             <i className="fas fa-pencil-alt mr-1" /> Skills
                                         </strong>
-                                        <p className="text-muted">
-                                            <span className="tag tag-danger">UI Design</span>
-                                            <span className="tag tag-success">Coding</span>
-                                            <span className="tag tag-info">Javascript</span>
-                                            <span className="tag tag-warning">PHP</span>
-                                            <span className="tag tag-primary">Node.js</span>
+                                        <p className="text-muted p-2">
+                                            <li className="tag tag-danger">UI Design</li>
+                                            <li className="tag tag-success">Coding</li>
+                                            <li className="tag tag-info">Javascript</li>
+                                            <li className="tag tag-warning">PHP</li>
+                                            <li className="tag tag-primary">Node.js</li>
                                         </p>
                                         <hr />
                                         <strong>
