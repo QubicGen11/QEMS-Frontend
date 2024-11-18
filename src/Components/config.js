df859b42b5f46ac30b32932cd3c7283a64acccd0
shaiksajidhussain <sanjusazid0@gmail.com>
2024-11-18 10:48:00 +0530

    Added the deployed urls

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiUrl: 'https://qubinest-ems-backend.vercel.app'
+    // apiUrl: 'https://qubinest-ems-backend.vercel.app'
+    apiUrl: 'https://qemsbe.qubinest.com/'
     // apiUrl: 'http://localhost:3000', // Localhost URL
   };
 
